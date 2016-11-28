4b6931f45b84fcb31656d17589afc4efc5a9ca79
Jasmine Greenaway <jasminegweb@gmail.com>
2016-11-28 11:58:24 -0500

    include test class to make sure name and value are the same

---
+++ ResourceValueTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+[Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+public class ResourceValueTest
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+    public void ValueAndNameAreTheSame()
+    {
+        ResourceSet resourceSet = MyResourceClass.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+        foreach (DictionaryEntry entry in resourceSet)
+        {
+            string resourceKey = entry.Key.ToString();
+            object resource = entry.Value;
+        }
+    }
+}
