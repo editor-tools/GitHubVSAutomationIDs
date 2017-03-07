d01df4b23c1e1e33da317ad1160d63f377969e76
Jamie Cansdale <jcansdale@gmail.com>
2017-03-07 15:16:14 +0000

    Split IDs and Tests projects

---
+++ GithubVSAutomationIDs.Tests/ResourceValueTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Globalization;
+using System.Resources;
+
+namespace GitHub.VisualStudio.TestAutomation
+{
+    [TestClass]
+    public class ResourceValueTest
+    {
+        [TestMethod]
+        public void ValueAndNameAreTheSame()
+        {
+            ResourceSet autoIDResourceSet = AutomationIDs.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+            foreach (DictionaryEntry entry in autoIDResourceSet)
+            {
+                var key = entry.Key.ToString();
+                var value = entry.Value.ToString();
+
+                Assert.AreEqual(value, key);
+            }
+        }
+    }
+}
